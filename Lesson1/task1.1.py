73f0f8c5ae4a0a73a91bd093ea4b93113b327a3f
Olenadovh <lothenshow@gmail.com>
2025-12-26 22:06:35 +0200

    Lesson 1 is redone

--- Lesson1/task1.1.py
+++ Lesson1/task1.1.py
@@ -1,19 +1,13 @@
-"""
-1) Write a function calculate_circle_area(radius) that:
-Takes the radius of a circle.
-Returns the area of a circle.
-Use this function in a program that asks the user for a radius and prints out the area.
-"""
 import math
 
 circle_radius = int(input("Enter radius of the circle\n"))
 
 
-def calculate_circle_area(radius: int|float) -> float:
+def calculate_circle_area(radius: int | float) -> float:
     """
     Returns the area of a circle based on the given radius
     """
-    return math.pi * radius**2
+    return math.pi * radius ** 2
 
 
 print(calculate_circle_area(circle_radius))
--- Lesson1/task1.2.py
+++ Lesson1/task1.2.py
@@ -1,40 +1,24 @@
-"""
-2) Create a Rectangle class that represents a rectangle.
-Class requirements:
-Class attributes:
-width — the width of the rectangle.
-height — the height of the rectangle.
-Class methods:
-__init__(self, width, height) — a constructor that accepts the width and height of the rectangle.
-area(self) — a method that returns the area of the rectangle.
-perimeter(self) — a method that returns the perimeter of the rectangle.
-is_square(self) — a method that returns True if the rectangle is a square (the width is equal to the height),
-otherwise False.
-resize(self, new_width, new_height) — a method that changes the width and height of the rectangle.
-Create an object of the Rectangle class and test all the methods.
-"""
-
-
 class Rectangle:
-    def __init__(self, width: int|float, height: int|float):
+    def __init__(self, width: int | float, height: int | float):
+        """Initializes a rectangle object with given parameters width, height"""
         if width <= 0 or height <= 0:
             raise ValueError("Rectangle side cannot be equal to or less than 0")
         self.width = width
         self.height = height
 
-    def area (self) -> int|float:
+    def area(self) -> int | float:
         """Returns the area of a rectangle by using its width and height"""
         return self.width * self.height
 
-    def perimeter(self) -> int|float:
+    def perimeter(self) -> int | float:
         """Returns the perimeter of a rectangle by using its width and height"""
         return 2 * (self.width + self.height)
 
-    def is_square(self) -> int|float:
+    def is_square(self) -> int | float:
         """Checks whether rectangle is square or not"""
         return self.width == self.height
 
-    def resize(self, new_width: int|float, new_height: int|float):
+    def resize(self, new_width: int | float, new_height: int | float):
         """Sets new width and height values"""
         if new_width <= 0 or new_height <= 0:
             raise ValueError("Rectangle side cannot be equal to or less than 0")
@@ -54,7 +38,7 @@ assert r1.perimeter() == 12, 'Test2'
 assert r1.is_square() == False, 'Test3'
 
 try:
-    r1.resize(8,0)
+    r1.resize(8, 0)
 except ValueError as e:
     print(e)
 
